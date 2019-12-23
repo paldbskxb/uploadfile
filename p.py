b339af7002aa83f2d0fe36d91467f2df78952294
paldbskxb <59151954+paldbskxb@users.noreply.github.com>
2019-12-23 08:27:22 +0800

    Add files via upload

---
+++ p.py
@@ -0,0 +1,21 @@
+print ('nothing here just go')
+
+lol = input ('apakah anda ingin melanjutkan : ')
+
+print ('apakah anda yakin menulis',lol)
+
+print ('ok')
+
+nama = input('apakah anda ingin menguatkan sinyal : ')
+
+print ('oke',nama)
+
+import os
+
+os.system ("ping 8.8.8.8")
+
+a = 0
+while (a < 3):
+	print ('yoooolooooo')
+	print ('udah jalan woy')
+	
